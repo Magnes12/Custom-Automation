42db9aac2de58749f4def86aa38f8c1a45de53ba
Damian <m4gn3s@gmail.com>
2024-02-12 11:12:53 +0100

    TODO

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import os 
+import os
 
 PATH = 'C:\\Users\\daszyma\\Downloads'
 
-files = os.listdir(PATH)
 
-for file in files:
-    extension = os.path.splitext(file)
-    if extension[1] == '.jnlp':
-        os.remove(os.path.join(PATH, file))
+def remove_files_by_extension(PATH):
+
+    files = os.listdir(PATH)
+    extensions = ('.jnlp', '.zip')
+
+    for file in files:
+        file_extension = os.path.splitext(file)
+        for extension in extensions:
+            if file_extension[1] == extension:
+                os.remove(os.path.join(PATH, file))
+
+
+if __name__ == "__main__":
+    remove_files_by_extension(PATH)
