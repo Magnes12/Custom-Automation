42c6505ff28916206df9ff30669ea71337b16de5
Damian <m4gn3s@gmail.com>
2024-02-12 12:45:55 +0100

    TODO

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
+import time
+import datetime
 
-PATH = 'C:\\Users\\daszyma\\Downloads'
+PATH = "C:\\Users\\daszyma\\Downloads"
+files = os.listdir(PATH)
 
 
-def remove_files_by_extension(PATH):
+def remove_files_by_extension(files):
 
-    files = os.listdir(PATH)
     extensions = ('.jnlp', '.zip')
 
     for file in files:
@@ -15,5 +17,26 @@ def remove_files_by_extension(PATH):
                 os.remove(os.path.join(PATH, file))
 
 
+def pdf_to_folder(files):
+
+    folder_name = "\\PDF_folder"
+    folder_path = PATH + folder_name
+
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_path)
+        time.sleep(1)
+    else:
+        for file in files:
+            file_extension = os.path.splitext(file)
+            if file_extension[1] == ".pdf":
+                os.rename(os.path.join(PATH, file), os.path.join(folder_path, file))
+
+
+# files = os.listdir(PATH)
+# for file in files:
+#     time_files = datetime.datetime.fromtimestamp(os.path.getatime(os.path.join(PATH, file)))
+#     print(time_files)
+
 if __name__ == "__main__":
-    remove_files_by_extension(PATH)
+    remove_files_by_extension(files)
+    pdf_to_folder(files)
